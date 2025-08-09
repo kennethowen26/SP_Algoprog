986a534ea156933d01c81c09bc068f3862e2295e
kennethowen26 <ken2owen2612@gmail.com>
2025-08-09 14:11:32 +0700

    Update Task26.cpp

--- Session22/26Task/Task26.cpp
+++ Session22/26Task/Task26.cpp
@@ -11,7 +11,6 @@ void bubbleSort(int arr[], int n) {
     for(int i = 0; i < n - 1; i++) {
         for(int j = 0; j < n - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
-                // Tukar elemen
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -41,4 +40,5 @@ int main() {
 
     return 0;
 }
+
 
